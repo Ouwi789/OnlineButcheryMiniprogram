3ada238dae219868fc4997abd164db5e4e9feec9
Ouwi789 <ouwi789@gmail.com>
2022-12-17 11:01:58 +1100

    Implemented sliders and used a for loop for onSale Container. Also added weui component

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,13 +4,35 @@ const app = getApp()
 
 Page({
   data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    buttons: [{text: '取消'}, {text: '确认'}],
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    wagyuSrc: 'https://cdn.shopify.com/s/files/1/0365/9327/0916/products/wagyu-fullblood-scotch-fillet-steak-marble-score-9-stone-axe-300g-vics-meat-668742_1400x.jpg?v=1658117888',
+    images: [{
+      src: 'https://cdn.shopify.com/s/files/1/0365/9327/0916/products/wagyu-fullblood-scotch-fillet-steak-marble-score-9-stone-axe-300g-vics-meat-668742_1400x.jpg?v=1658117888'
+    }, {
+      src: 'https://awmgroup.com.au/wp-content/uploads/2016/05/lamb-leg.png'
+    }, {
+      src: 'https://www.thehappychickencoop.com/wp-content/uploads/2022/01/white-striping-in-chicken-meat.jpg'
+    }],
     searchSrc: 'https://cdn.icon-icons.com/icons2/2066/PNG/512/search_icon_125165.png',
-    categorySrc: 'https://thenounproject.com/api/private/icons/323575/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjjteH2eBWNz9IRYUIEUZT2d1C_BSCnn0jyi0z8-KcjTg7YqDV1W5GL2pnkP11kQbRdDW6d2WAmsftqwvICJfdDQbVGw%3D%3D'
+    categorySrc: 'https://thenounproject.com/api/private/icons/323575/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0&token=gAAAAABjjteH2eBWNz9IRYUIEUZT2d1C_BSCnn0jyi0z8-KcjTg7YqDV1W5GL2pnkP11kQbRdDW6d2WAmsftqwvICJfdDQbVGw%3D%3D',
+    imageArray: [{
+      //TODO seperate image src with positioning of top and left
+      src: 'a', top: 55, left: 5
+    }, {
+      src:'b', top: 55, left: 37.5
+    }, {
+      src:'c', top:55, left: 69.5
+    }, {
+      src:'d', top:75, left:5
+    }, {
+      src:'e', top:75, left:37.5
+    }, {
+      src:'f', top:75, left:69.5
+    }]
   },
   //事件处理函数
   bindViewTap: function() {
